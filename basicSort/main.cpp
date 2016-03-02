a5c91ba4ebcc9db61534a8b2f91757168916ff10
francesca-figueroa <isabella.figueroa@colorado.edu>
2016-03-02 11:10:37 -0700

    Initial Commit

---
+++ basicSort/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  basicSort
+//
+//  Created by Francesca Figueroa on 3/2/16.
+//  Copyright © 2016 Isabella Figueroa. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
