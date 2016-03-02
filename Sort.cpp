ad9a848b97b54a080952936effa080af546f033f
francesca.figueroa <isabella.figueroa@colorado.edu>
2016-03-02 11:26:59 -0700

    Removed unnecessary files

--- Sort.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  Sort.cpp
-//  basicSort
-//
-//  Created by Francesca Figueroa on 3/2/16.
-//  Copyright © 2016 Isabella Figueroa. All rights reserved.
-//
-
-#include "Sort.hpp"
--- basicSort/sorting.cpp
+++
@@ -1,9 +0,0 @@
-//
-//  sorting.cpp
-//  basicSort
-//
-//  Created by Francesca Figueroa on 3/2/16.
-//  Copyright © 2016 Isabella Figueroa. All rights reserved.
-//
-
-#include "sorting.hpp"
